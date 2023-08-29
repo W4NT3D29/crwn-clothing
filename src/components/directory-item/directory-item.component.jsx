fdb8e8be08ac322881dbd7834255e6451f874227
Omar Figueroa <38760041+W4NT3D29@users.noreply.github.com>
2023-08-29 14:19:05 -0600

    Finished touches and links for everything to work propperly

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import "./directory-item.styles.scss";
+
+const DirectoryItem = ({ category: { imageUrl, title, route } }) => {
+    const navigate = useNavigate();
+    const onNavigateHandler = () => navigate(route);
+    return (
+        <div className="directory-item-container" onClick={onNavigateHandler}>
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+            />
+            <div className="directory-body">
+                <h2>{title.toUpperCase()}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    );
+};
+
+export default DirectoryItem;
